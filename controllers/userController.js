defebcc78955af0b991c02ab9c8d69954a18ec15
Gururaj2004 <guruguru6631@gmail.com>
2024-06-02 13:29:48 +0530

    host final

---
+++ controllers/userController.js
@@ -0,0 +1,64 @@
+const User = require("../models/UserModel");
+
+module.exports.getLikedMovies = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.json({ msg: "success", movies: user.likedMovies });
+    } else return res.json({ msg: "User not found." });
+  } catch (error) {
+    return res.json({ msg: "Error fetching movies." });
+  }
+};
+
+module.exports.addToLikedMovies = async (req, res) => {
+  try {
+    const { email, data } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const { likedMovies } = user;
+      const movieExists = likedMovies.find(({ id }) => id === data.id);
+      if (!movieExists) {
+        await User.findByIdAndUpdate(
+          user._id,
+          {
+            likedMovies: [...user.likedMovies, data],
+          },
+          { new: true }
+        );
+      } else return res.json({ msg: "Movie already added." });
+    } else await User.create({ email, likedMovies: [data] });
+    return res.json({ msg: "Movie added successfully." });
+  } catch (error) {
+    return res.json({ msg: "Error adding movie." });
+  }
+};
+
+module.exports.removeFromLikedMovies = async (req, res) => {
+  try {
+    const { email, movieId } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      const movies = user.likedMovies;
+      const movieIndex = movies.findIndex(({ id }) => id === movieId);
+      if (movieIndex === -1) {
+        return res.status(400).send({ msg: "Movie not found." });
+      }
+      const updatedMovies = movies.filter(({ id }) => id !== movieId);
+      await User.findByIdAndUpdate(
+        user._id,
+        {
+          likedMovies: updatedMovies,
+        },
+        { new: true }
+      );
+      return res.json({
+        msg: "Movie removed successfully.",
+        movies: updatedMovies,
+      });
+    } else return res.json({ msg: "User not found." });
+  } catch (error) {
+    return res.json({ msg: "Error removing movie." });
+  }
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const userRoutes = require("./routes/UserRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.error("DB Connection Error:", err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
